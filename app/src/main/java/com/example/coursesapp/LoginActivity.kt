b3ef490cd4221727ea7141fce7393d7a3efcfd8a
Armando Beltran <beltran.armando2210@gmail.com>
2023-06-29 21:48:44 -0600

    first commit

---
+++ app/src/main/java/com/example/coursesapp/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.coursesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class LoginActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+    }
+}
