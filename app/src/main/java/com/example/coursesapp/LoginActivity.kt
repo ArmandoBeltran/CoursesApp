4dbb4c0b0d2e546a3dd20bf7dc229d19cde0f8cf
Armando Beltran <beltran.armando2210@gmail.com>
2023-06-29 22:43:02 -0600

    Se agrega la pantalla de signup, y se cambia el nombre de algunos ids que estaban erroneos en la patanlla de login

--- app/src/main/java/com/example/coursesapp/LoginActivity.kt
+++ app/src/main/java/com/example/coursesapp/LoginActivity.kt
@@ -1,13 +1,22 @@
 package com.example.coursesapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class LoginActivity: ComponentActivity() {
+    lateinit var not_account : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_login)
+
+        not_account = findViewById(R.id.login_signup)
+        not_account.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/coursesapp/SignUpActivity.kt
@@ -0,0 +1,14 @@
+package com.example.coursesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+
+class SignUpActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_sign_up)
+    }
+}
