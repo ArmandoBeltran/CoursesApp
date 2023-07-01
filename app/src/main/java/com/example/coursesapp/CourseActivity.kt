f78606f4b69f8ba161d1788d3058628d871655f4
Armando Beltran <beltran.armando2210@gmail.com>
2023-07-01 01:51:20 -0600

    Se agregan las pantallas de la lista de cursos y del curso, falta mejorar la navegacion, y agregar una barra de navegacion

---
+++ app/src/main/java/com/example/coursesapp/CourseActivity.kt
@@ -0,0 +1,13 @@
+package com.example.coursesapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class CourseActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_course)
+    }
+}
